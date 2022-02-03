c3812fe14d1730036e203ab4577dff0557a4ffa1
Piotr Machnicki <piotrmachnicki1989@gmail.com>
2022-02-03 16:52:04 +0100

    add proper error handling middleware and template

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const csrf = require("csurf");
 
 const db = require("./database/database");
 const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
+const errorHandleMiddleware = require("./middlewares/error-handler");
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
@@ -19,6 +20,8 @@ app.use(csrf());
 app.use(addCsrfTokenMiddleware);
 
 app.use(authRoutes);
+
+app.use(errorHandleMiddleware);
 
 db.connectToDatabase()
   .then(function () {
