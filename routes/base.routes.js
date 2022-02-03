dba3eff33e5eea7ff2afd158485111097cbb40ab
Piotr Machnicki <piotrmachnicki1989@gmail.com>
2022-02-03 20:13:50 +0100

    little base rout path

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -3,7 +3,7 @@ const res = require("express/lib/response");
 
 const router = express.Router();
 
-router.get("/", function () {
+router.get("/", function (req, res) {
   res.redirect("/products");
 });
 
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -3,7 +3,7 @@ const res = require("express/lib/response");
 
 const router = express.Router();
 
-router.get("/products", function () {
+router.get("/products", function (req, res) {
   res.render("customer/products/all-products");
 });
 
